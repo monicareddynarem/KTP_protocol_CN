a988aed3377371eac35a2acd3267a9341361b4eb
monicareddynarem <monicareddynarem@gmail.com>
2026-03-08 15:44:43 +0530

    changed struct

--- initksocket.c
+++ initksocket.c
@@ -2,26 +2,36 @@
 #include<sys/types.h>
 
 #define N 100
+#define SEND_BUF_SIZE 100
+#define RECV_BUF_SIZE 10
 
-typedef struct swnd{
-    int sender_ws;
-    int* msgs;
-}swnd;
+//still need to improve
+typedef struct swnd_struct{
+    int swnd_size;
+    int unacked[10];
+}swnd_struct;
 
-typedef struct rwnd{
-    int receiver_ws;
-    int* msgs;
-}rwnd;
+typedef struct rwnd_struct{
+    int rwnd_size;
+    int expected[10];
+}rwnd_struct;
 
+
+typedef struct message{
+    int seq_no;
+    char msg_data[512];//each msg is 512 bytes(fixed)
+}message;
 
 typedef struct sock_info{
     int free;//1 if free, 0 if not free
     pid_t ppid;//parent process pid
     int fd_udp;//underlying udp socket
-    char IP[50];//other end IP
+    char* IP;//other end IP
     int port;//other end port
-    int* send_buffer;
-    int* recv_buffer;
+    message send_buffer[SEND_BUF_SIZE];//fixed size array of messages
+    message recv_buffer[RECV_BUF_SIZE];
+    swnd_struct swnd;
+    rwnd_struct rwnd;
 }sock_info;
 
 void garbage_collecter(){
@@ -31,7 +41,8 @@ void garbage_collecter(){
 
 
 int main(){
-    
+    //implement 2 threads R and S
+
     int shmid=shmget(100,N*sizeof(sock_info),0);
 
 
