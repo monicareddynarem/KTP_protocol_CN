289e1be2ecb159198a37a8ad0cbc3f61ecf4505c
monicareddynarem <monicareddynarem@gmail.com>
2026-03-10 21:26:59 +0530

    git

--- ksocket.h
+++ ksocket.h
@@ -10,3 +10,14 @@
 
 int errno = 0;
 
+
+int k_socket(int domain, int type, int protocol);
+int k_bind(int sock_KTP, char* src_IP, int src_port, char* dest_IP, int dest_port);
+int k_sendto(int sock_KTP, const void buf[.size], size_t size, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
+int k_recvfrom(int sock_KTP, const void buf[.size], size_t size, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
+int k_close(int sock_ktp);
+int drop_message(float p);
+
+
+
+
---
+++ user2.c
@@ -0,0 +1,21 @@
+#include<sys/shm.h>
+#include<sys/types.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include<ksocket.h>
+
+//receiver
+
+int main(){
+    int M2=k_socket(AF_INET,SOCK_KTP,)
+    
+
+
+    return 0;
+}
